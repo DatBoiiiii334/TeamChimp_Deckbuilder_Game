731953b4126ff244d6e05d33b5238cccce7bdf32
Chris <chrisaconquet@hotmail.com>
2021-10-12 11:35:12 +0100

    Added card display
    1. New card display has been added
    - You can now pick a new card to add to your deck
    - cardpicker.cs controls manages the card profiles you dont use
    - displaycard.cs is the script given to the cards put on display, on select the script adds the new card profile to your carddeck and removes it from the crdpicker list.
    
    (WARNING: cardprofiles you add to your deck dont stay there after you have exit play mode, they all return back to normal
    
    you have to find a way to save your progress and card deck in game.

---
+++ Assets/Scripts/CardPicker.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPicker : MonoBehaviour
+{
+    public static CardPicker instance_CardPicker;
+    public GameObject CardPrefab, CardSpawnPoint, CardDisplay;
+    public List<Card> AllNewCardProfiles;
+
+    private void Awake()
+    {
+        if (instance_CardPicker != null)
+        {
+            Destroy(gameObject);
+        }
+        instance_CardPicker = this;
+    }
+    
+
+    public void OpenNewCardsWindow()
+    {
+        CardDisplay.SetActive(true);
+        PlaceNewCards();
+    }
+
+    public void CloseNewCardsWindow()
+    {
+        CardDisplay.SetActive(false);
+        GameManager._instance.RemoveCards(CardSpawnPoint.transform);
+    }
+
+    public void PlaceNewCards()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            int randomvalue;
+            GameObject myCard;
+            randomvalue = Random.Range(0, AllNewCardProfiles.Count);
+            myCard = Instantiate(CardPrefab, CardSpawnPoint.transform);
+            myCard.GetComponent<DisplayCard>().card = AllNewCardProfiles[randomvalue];
+        }
+    }
+}
